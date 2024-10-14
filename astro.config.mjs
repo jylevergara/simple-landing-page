22cdcbb0005a4064e99efae3617e8328bb788571
Jyle Vergara <jylevergara@gmail.com>
2024-10-13 20:33:56 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-simple-portfolio.vercel.app/', // Required for sitemap -> Replace with your site's URL
+	site: 'https://jylevergara.github.io/', // Required for sitemap -> Replace with your site's URL
 	output: 'hybrid',
 	integrations: [tailwind(), sitemap()],
 	adapter: vercel(),
