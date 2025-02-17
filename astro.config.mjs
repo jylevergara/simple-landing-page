fa130952ea232426c237686def6dd6cf28145e89
Jyle Vergara <jylevergara@gmail.com>
2025-02-17 18:32:38 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({
-	site: 'https://jylevergara.github.io/', // Required for sitemap -> Replace with your site's URL
-	output: 'static',
-	integrations: [tailwind(), sitemap()],
-});
+// // https://astro.build/config
+// export default defineConfig({
+// 	site: 'https://jylevergara.github.io/', // Required for sitemap -> Replace with your site's URL
+// 	output: 'static',
+// 	integrations: [tailwind(), sitemap()],
+// });
